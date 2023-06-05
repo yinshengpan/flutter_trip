0b1b91e4b3afd1e1acc690205686226bcd8f3675
尹晟攀 <shengpan.yin@eezi-tech.com>
2023-06-05 18:01:12 +0800

    feat:首页增加banner样式

--- lib/home/HomePage.dart
+++ lib/home/HomePage.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper_view/flutter_swiper_view.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
+List _imageList = [
+  "https://picsum.photos/250?image=9",
+  "http://www.devio.org/img/avatar.png",
+  "https://picsum.photos/250?image=9",
+  "http://www.devio.org/img/avatar.png"
+];
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("首页"),
+    return Center(
+      child: Column(
+        children: [
+          Container(
+              height: 220,
+              child: Swiper(
+                itemCount: _imageList.length,
+                autoplay: true,
+                pagination: SwiperPagination(),
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(
+                    _imageList[index],
+                    fit: BoxFit.fill,
+                  );
+                },
+              ))
+        ],
       ),
     );
   }
