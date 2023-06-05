a79b90c56195622b9c53e4588cd26b2c146b8989
尹晟攀 <shengpan.yin@eezi-tech.com>
2023-06-05 17:46:03 +0800

    feat:搭建首页基础框架

---
+++ lib/navigator/TabNavigator.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_trip/home/HomePage.dart';
+import 'package:flutter_trip/mine/MinePage.dart';
+import 'package:flutter_trip/search/SearchPage.dart';
+import 'package:flutter_trip/travel/TravelPage.dart';
+
+class TabNavigator extends StatefulWidget {
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+  final _defaultColor = Colors.grey;
+  final _activeColor = Colors.blue;
+  int _currentIndex = 0;
+  final PageController _controller = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _controller,
+        children: [HomePage(), SearchPage(), TravelPage(), MinePage()],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          _controller.jumpToPage(index);
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: _activeColor,
+        unselectedItemColor: _defaultColor,
+        selectedFontSize: 15,
+        unselectedFontSize: 15,
+        items: [
+          _createBottomItem(Icons.home, "首页"),
+          _createBottomItem(Icons.search, "搜索"),
+          _createBottomItem(Icons.camera_alt, "旅拍"),
+          _createBottomItem(Icons.account_circle, "我的"),
+        ],
+      ),
+    );
+  }
+
+  _createBottomItem(IconData? icon, String title) {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        icon,
+        color: _defaultColor,
+      ),
+      activeIcon: Icon(
+        icon,
+        color: _activeColor,
+      ),
+      label: title,
+    );
+  }
+}
