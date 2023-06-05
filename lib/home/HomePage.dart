883ff7be78dfe2631fba619b347fabcb93cfeefa
尹晟攀 <shengpan.yin@gmail.com>
2023-06-05 21:24:32 +0800

    feat:首页顶部滑动渐变

--- lib/home/HomePage.dart
+++ lib/home/HomePage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper_view/flutter_swiper_view.dart';
+import 'dart:math' as math;
+
+const APPBAR_MAX_SCROLL = 100;
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,26 +17,74 @@ List _imageList = [
 ];
 
 class _HomePageState extends State<HomePage> {
+  double _appBarAlpha = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
+    return Scaffold(
+      body: Stack(
         children: [
-          Container(
-              height: 220,
-              child: Swiper(
-                itemCount: _imageList.length,
-                autoplay: true,
-                pagination: SwiperPagination(),
-                itemBuilder: (BuildContext context, int index) {
-                  return Image.network(
-                    _imageList[index],
-                    fit: BoxFit.fill,
-                  );
+          MediaQuery.removePadding(
+              context: context,
+              removeTop: true,
+              child: NotificationListener(
+                onNotification: (scroll) {
+                  if (scroll is ScrollUpdateNotification && scroll.depth == 0) {
+                    _onScroll(scroll.metrics.pixels);
+                    return true;
+                  }
+                  return false;
                 },
-              ))
+                child: ListView(
+                  children: [
+                    Container(
+                        height: 220,
+                        child: Swiper(
+                          itemCount: _imageList.length,
+                          autoplay: true,
+                          pagination: SwiperPagination(),
+                          itemBuilder: (BuildContext context, int index) {
+                            return Image.network(
+                              _imageList[index],
+                              fit: BoxFit.fill,
+                            );
+                          },
+                        )),
+                    Container(
+                      height: 800,
+                      child: ListTile(
+                        title: Text("哈哈"),
+                      ),
+                    )
+                  ],
+                ),
+              )),
+          Opacity(
+            opacity: _appBarAlpha,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text("首页"),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  void _onScroll(double pixels) {
+    setState(() {
+      double alpha = pixels / APPBAR_MAX_SCROLL;
+      alpha = math.max(0, alpha);
+      alpha = math.min(1, alpha);
+      setState(() {
+        _appBarAlpha = alpha;
+      });
+    });
   }
 }
